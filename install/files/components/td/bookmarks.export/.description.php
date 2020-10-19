560d61f0bd01f66b9b2fce01c394310cd7db40c4
chirikova <t.desyatkina@gmail.com>
2020-10-19 11:32:15 +0500

    component descr fix

--- install/files/components/td/bookmarks.export/.description.php
+++ install/files/components/td/bookmarks.export/.description.php
@@ -6,7 +6,6 @@ use Bitrix\Main\Localization\Loc;
 $arComponentDescription = array(
 	"NAME" => Loc::getMessage("TDB_BOOKMARKS_NAME"),
 	"DESCRIPTION" => Loc::getMessage("TDB_BOOKMARKS_DESCRIPTION"),
-	"COMPLEX" => "Y",
 	"PATH" => array(
 		"ID" => "TDB",
 		"NAME" => Loc::getMessage("TDB_GROUP_NAME")
--- install/files/components/td/bookmarks.export/lang/ru/.description.php
+++ install/files/components/td/bookmarks.export/lang/ru/.description.php
@@ -2,5 +2,5 @@
 defined('B_PROLOG_INCLUDED') || die;
 
 $MESS['TDB_GROUP_NAME'] = "Модуль закладки";
-$MESS['TDB_BOOKMARKS_NAME'] = 'Закладки';
-$MESS['TDB_BOOKMARKS_DESCRIPTION'] = 'Закладки: комплексный компонент';
+$MESS['TDB_BOOKMARKS_NAME'] = 'Закладки экспорт';
+$MESS['TDB_BOOKMARKS_DESCRIPTION'] = 'Закладки: экспорт';
